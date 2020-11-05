4a7939cdcdfe9993b0f4694977a1070624c9b123
Danil Shvalov <shvalovich@gmail.com>
2020-11-05 20:22:55 +1000

    Рабочая версия

--- final-first/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/danilshvalov/dev/c-plus-plus/c-plus-plus-learning/final-first
-# Build directory: /home/danilshvalov/dev/c-plus-plus/c-plus-plus-learning/final-first/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
