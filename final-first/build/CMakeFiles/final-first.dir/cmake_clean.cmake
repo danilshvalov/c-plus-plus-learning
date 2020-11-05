f2c8f0cc38052c2c055b0231f03ebb05c0b9ef63
Danil Shvalov <shvalovich@gmail.com>
2020-11-05 13:58:33 +1000

    Развиваемся..

---
+++ final-first/build/CMakeFiles/final-first.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/final-first.dir/date.cpp.o"
+  "CMakeFiles/final-first.dir/main.cpp.o"
+  "final-first"
+  "final-first.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/final-first.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
